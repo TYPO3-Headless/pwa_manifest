26f735db5185722231f523b5e9d83669fbcdfc77
Tymoteusz Motylewski <t.motylewski@gmail.com>
2020-01-18 14:51:16 +0100

    Update TYPO3 dependencies to cover v10LTS

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,8 +11,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.0.99',
-            'frontend' => '9.5.0-10.0.99'
+            'typo3' => '9.5.0-10.4.99',
+            'frontend' => '9.5.0-10.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
